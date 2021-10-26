99ede43790f2e5623fcb4c5b6b53c78b994c2f19
Bhupesh Dhapola <82699890+BhupeshDhapola@users.noreply.github.com>
2021-10-26 12:08:38 +0530

    Update bhupesh.java

--- bhupesh.java
+++ bhupesh.java
@@ -1,6 +1,6 @@
 public class bhupesh {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Hello");
+        System.out.println("Hello Mates");
     }
 }
